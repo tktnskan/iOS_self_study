b8f0a4b77ef301d79470f50c3b4ae4dd82a2cd84
tktnskan <jny4594@gmail.com>
2021-12-08 22:46:52 +0900

    SwiftUI pageViewController 공부중

---
+++ SwiftUITutorial/SwiftUITutorial/Views/PageView/PageView.swift
@@ -0,0 +1,23 @@
+//
+//  PageView.swift
+//  SwiftUITutorial
+//
+//  Created by Jinyung Yoon on 2021/12/08.
+//
+
+import SwiftUI
+
+struct PageView<Page: View>: View {
+    var pages: [Page]
+    
+    var body: some View {
+        PageViewController(pages: pages)
+    }
+}
+
+struct PageView_Previews: PreviewProvider {
+    static var previews: some View {
+        PageView(pages: ModelData().features.map { FeatureCard(landmark: $0) })
+            .aspectRatio(3 / 2, contentMode: .fit)
+    }
+}
