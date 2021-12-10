29ef3fe68b28856f353289dd77da8253753f041b
tktnskan <jny4594@gmail.com>
2021-12-10 16:33:22 +0900

    initial commit
    로그인 페이지 생성 + 파이어베이스 연동

---
+++ ChattingAppWithSwiftUI/ChattingAppWithSwiftUI/ChattingAppWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChattingAppWithSwiftUIApp.swift
+//  ChattingAppWithSwiftUI
+//
+//  Created by GJC03280 on 2021/12/10.
+//
+
+import SwiftUI
+
+@main
+struct ChattingAppWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView()
+        }
+    }
+}
---
+++ ChattingAppWithSwiftUI/ChattingAppWithSwiftUI/LoginView.swift
@@ -0,0 +1,128 @@
+//
+//  ContentView.swift
+//  ChattingAppWithSwiftUI
+//
+//  Created by GJC03280 on 2021/12/10.
+//
+
+import SwiftUI
+import Firebase
+
+class FirebaseManager: NSObject {
+    
+    let auth: Auth
+    
+    static let shared = FirebaseManager()
+    
+    override init() {
+        FirebaseApp.configure()
+        self.auth = Auth.auth()
+        super.init()
+    }
+}
+
+struct LoginView: View {
+    
+    @State var isLoginMode = false
+    @State var email = ""
+    @State var password = ""
+    
+    var body: some View {
+        NavigationView {
+            ScrollView {
+                VStack(spacing: 16) {
+                    Picker(selection: $isLoginMode, label: Text("Picker here")) {
+                        Text("Login")
+                            .tag(true)
+                        Text("Create Account")
+                            .tag(false)
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    if !isLoginMode {
+                        Button{
+                            
+                        } label: {
+                            Image(systemName: "person.fill")
+                                .font(.system(size: 64))
+                                .padding()
+                        }
+                    }
+                    
+                    Group {
+                        TextField("Email", text: $email)
+                            .keyboardType(.emailAddress)
+                            .autocapitalization(.none)
+                        SecureField("Password", text: $password)
+                    }
+                    .padding(12)
+                    .background(Color.white)
+                    
+                    Button{
+                        handleAction()
+                    } label: {
+                        HStack {
+                            Spacer()
+                            Text(isLoginMode ? "Login" : "Create Account")
+                                .foregroundColor(.white)
+                                .padding(.vertical, 10)
+                                .font(.system(size: 14, weight: .semibold))
+                            Spacer()
+                        }
+                        .background(Color.blue)
+                    }
+                    
+                    Text(self.loginStatusMessage)
+                        .foregroundColor(.red)
+                }
+                .padding()
+            }
+            .navigationTitle(isLoginMode ? "Login" : "Create Account")
+            .background(Color(.init(white: 0, alpha: 0.05))
+                            .ignoresSafeArea()
+            )
+        }
+        .navigationViewStyle(.stack)
+    }
+    
+    private func handleAction() {
+        if isLoginMode {
+            print("Should log into Firebase with existing credentials")
+            loginUser()
+        } else {
+            createNewAccount()
+        }
+    }
+    
+    @State var loginStatusMessage = ""
+    
+    private func createNewAccount() {
+        FirebaseManager.shared.auth.createUser(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("Failed to create user: ", error)
+                self.loginStatusMessage = "Failed to create user: \(error)"
+                return
+            }
+            print("Successfully created user: \(result?.user.uid ?? "")")
+            self.loginStatusMessage = "Successfully created user: \(result?.user.uid ?? "")"
+        }
+    }
+    
+    private func loginUser() {
+        FirebaseManager.shared.auth.signIn(withEmail: email, password: password) { result, error in
+            if let error = error {
+                print("Failed to login user: ", error)
+                self.loginStatusMessage = "Failed to login user: \(error)"
+                return
+            }
+            print("Successfully logged in as user: \(result?.user.uid ?? "")")
+            self.loginStatusMessage = "Successfully logged in as user: \(result?.user.uid ?? "")"
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
